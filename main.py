969f596e050b653274e11b537af03d42cc947cb4
Nishad A <abdulnishad12@gmail.com>
2016-12-12 16:18:04 +0530

    commit

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 #created by Nishad A
 #copyright protected
-import nltk
-msg=input(">: ")
-print ("Hi whats your name?")
+import aiml
+import os
+kernel = aiml.Kernel()
+#if os.path.isfile("krips.brn"):
+ #   kernel.bootstrap(brainFile = "krips.brn")
+#else:
+kernel.bootstrap(learnFiles = "krips.xml", commands = "LOAD AIML B")
+kernel.saveBrain("krips.brn")
+
+while True:
+    print(kernel.respond(raw_input(">: ")))
+    
